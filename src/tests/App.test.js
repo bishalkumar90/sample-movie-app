d9ce6f85d99d4b0ee6d759c37aecb8f3ae9b67a8
Bishal Kumar <fptbishalkumar@gmail.com>
2021-10-18 19:08:34 +0700

    add test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,40 @@
-test("it should render the correct selected item", () => {
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import MetasComponent from "../components/Metas/MetasComponent";
+import CarouselComponent from "../components/Carousel/CarouselComponent";
+import mockData from "./mock/data";
 
+const listGenre = [
+  ...new Set(
+    mockData
+      .reduce((acc, cur) => `${acc ? acc + ", " : ""}${cur.Genre}`, "")
+      .split(", ")
+  ),
+];
+
+test("it should render the correct selected item", () => {
+  render(<MetasComponent movie={mockData[0]} />);
+  const titleElem = screen.getAllByText(mockData[0].Title);
+  titleElem.forEach((item) => {
+    expect(item).toBeInTheDocument();
+  });
+});
+
+test("it should render the correct list genre", () => {
+  render(<CarouselComponent movies={mockData} genres={listGenre} />);
+  listGenre.forEach((genre) => {
+    const genreElem = screen.getByText(genre);
+    expect(genreElem).toBeInTheDocument();
+  });
+});
+
+test("it should render the correct list movies", () => {
+  render(<CarouselComponent movies={mockData} genres={listGenre} />);
+  const posters = screen.getAllByAltText(/movie poster/i);
+
+  mockData.forEach((movie, index) => {
+    const srcElemText = posters[index].getAttribute("src");
+    expect(srcElemText).toEqual(movie.Poster);
+  });
 });
